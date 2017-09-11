f76554a2f10ac79d5bcebb7f0a331b1d99152387
ms-dev <dev@mingsoft.net>
2017-09-11 11:49:43 +0800

    更新springutil类

--- src/main/java/net/mingsoft/base/util/SpringUtil.java
+++ src/main/java/net/mingsoft/base/util/SpringUtil.java
@@ -1,29 +1,34 @@
 
 package net.mingsoft.base.util;
 
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-
 public class SpringUtil {
-	
+
+	// private static ThreadLocal<HttpServletRequest> requestLocal = new
+	// ThreadLocal<HttpServletRequest>();
+	private static ThreadLocal<HttpServletResponse> responseLocal = new ThreadLocal<HttpServletResponse>();
+
 	/**
 	 * 获取当前请求对象
+	 * 
 	 * @return
 	 */
-	public static HttpServletRequest getRequest(){
-		try{
-			return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
-		}catch(Exception e){
+	public static HttpServletRequest getRequest() {
+		try {
+			return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		} catch (Exception e) {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * 通过spring的webapplicationcontext上下文对象读取bean对象
 	 * 
@@ -47,9 +52,10 @@ public class SpringUtil {
 	 * @return 返回获取到的对象。获取不到返回null
 	 */
 	public static Object getBean(String beanName) {
-		return WebApplicationContextUtils.getWebApplicationContext(SpringUtil.getRequest().getServletContext()).getBean(beanName);
+		return WebApplicationContextUtils.getWebApplicationContext(SpringUtil.getRequest().getServletContext())
+				.getBean(beanName);
 	}
-	
+
 	/**
 	 * 通过spring的webapplicationcontext上下文对象读取bean对象
 	 * 
@@ -59,7 +65,20 @@ public class SpringUtil {
 	 *            要读取的类名称
 	 * @return 返回获取到的对象。获取不到返回null
 	 */
-	public static <T>T getBean(Class<T> cls) {
-		return WebApplicationContextUtils.getWebApplicationContext(SpringUtil.getRequest().getServletContext()).getBean(cls);
+	public static <T> T getBean(Class<T> cls) {
+		return WebApplicationContextUtils.getWebApplicationContext(SpringUtil.getRequest().getServletContext())
+				.getBean(cls);
+	}
+
+	public static HttpServletResponse getResponse() {
+		return (HttpServletResponse) responseLocal.get();
+	}
+
+	public static void setResponse(HttpServletResponse response) {
+		responseLocal.set(response);
+	}
+
+	public static HttpSession getSession() {
+		return SpringUtil.getRequest().getSession();
 	}
 }
